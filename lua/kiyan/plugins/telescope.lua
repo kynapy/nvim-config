250af9f9cd728f63bf8629305fe2b972a2fe52bf
Kiyan <apyoung11@gmail.com>
2025-07-07 04:14:30 +0800

    Fix Trouble and swap Trouble and Telescope integration to Telescope
    The Trouble integration config code was causing Trouble to not load
    properly. Moved the code over to Telescope for now, but yet to test
    integration.

--- lua/kiyan/plugins/telescope.lua
+++ lua/kiyan/plugins/telescope.lua
@@ -18,11 +18,25 @@ return {
         vim.keymap.set('n', '<leader>ft', "<cmd>TodoTelescope<cr>", { desc = "Find Todos" })
 
         telescope.load_extension("fzf")
+
+        -- For trouble
+        local actions = require("telescope.actions")
+        local open_with_trouble = require("trouble.sources.telescope").open
+
+        -- Use this to add more results without clearing the trouble list
+        local add_to_trouble = require("trouble.sources.telescope").add
+
         telescope.setup({
             defaults = {
                 file_ignore_patterns = {
                     "node_modules"
-                }
+                },
+
+                -- For trouble
+                mappings = {
+                    i = { ["<c-t>"] = open_with_trouble },
+                    n = { ["<c-t>"] = open_with_trouble },
+                },
             }
         })
     end
--- lua/kiyan/plugins/trouble.lua
+++ lua/kiyan/plugins/trouble.lua
@@ -4,7 +4,7 @@ return {
     cmd = "Trouble",
     keys = {
         {
-            "<leader>xx",
+           "<leader>xx",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
         },
@@ -34,22 +34,4 @@ return {
             desc = "Quickfix List (Trouble)",
         },
     },
-    config = function ()
-        local actions = require("telescope.actions")
-        local open_with_trouble = require("trouble.sources.telescope").open
-
-        -- Use this to add more results without clearing the trouble list
-        local add_to_trouble = require("trouble.sources.telescope").add
-
-        local telescope = require("telescope")
-
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = { ["<c-t>"] = open_with_trouble },
-                    n = { ["<c-t>"] = open_with_trouble },
-                },
-            },
-        })
-    end
 }
