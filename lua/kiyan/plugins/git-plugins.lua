2af7e8ac3bf485f6a951d67ec66fba6d366bb872
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2025-11-15 16:47:07 +0800

    Consolidate Git related plugins

---
+++ lua/kiyan/plugins/git-plugins.lua
@@ -0,0 +1,28 @@
+return {
+    -- Git Signs
+    {
+        'https://github.com/lewis6991/gitsigns.nvim',
+        config = function ()
+            local gitsigns = require('gitsigns')
+
+            gitsigns.setup({
+                -- Add configuration here
+            })
+
+            vim.keymap.set('n', '<leader>gb', gitsigns.blame, { desc = "Git Blame" })
+            vim.keymap.set('n', '<leader>gd', gitsigns.diffthis, { desc = "Git Diff" })
+
+            -- Toggles for gitsigns
+            vim.keymap.set('n', '<leader>gtb', gitsigns.toggle_current_line_blame, { desc = "Git Toggle Current Line Blame" })
+            vim.keymap.set('n', '<leader>gtd', gitsigns.toggle_word_diff, { desc = "Git Toggle Word Diff" })
+            vim.keymap.set('n', '<leader>gth', gitsigns.preview_hunk, { desc = "Git Toggle Hunk" })
+        end
+    },
+    -- VIM Fugitive
+    {
+        'tpope/vim-fugitive',
+        config = function()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git Status" })
+        end
+    }
+}
