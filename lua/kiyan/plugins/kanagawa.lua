2def2e597c23f4677bdcb5f76e9b47484f95271b
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2025-11-15 16:58:28 +0800

    Change theme and remove italics for comments

--- lua/kiyan/plugins/kanagawa.lua
+++ lua/kiyan/plugins/kanagawa.lua
@@ -3,6 +3,8 @@ return {
     config = function ()
         require('kanagawa').setup({})
 
-        vim.cmd.colorscheme('kanagawa')
+        vim.cmd.colorscheme('kanagawa-dragon')
+        local set_hl = vim.api.nvim_set_hl
+        set_hl(0, "Comment", { italic = false })
     end
 }
