5de78e7c9d4d3ac8386c6e6a363b076431f14c5d
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2025-11-17 16:38:39 +0800

    Update Conform plugin to make it work with commonly used file types

--- lua/kiyan/plugins/conform.lua
+++ lua/kiyan/plugins/conform.lua
@@ -1,18 +1,28 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function ()
-        require("conform").setup({
-            lua = { 'stylua' },
-            javascript = { 'prettier', 'eslint', 'ts_ls', stop_after_first = true },
-        })
+	-- Formatter plugin for Neovim
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier", stop_after_first = true },
+				typescript = { "prettier", stop_after_first = true },
+				javascriptreact = { "prettier", stop_after_first = true },
+				typescriptreact = { "prettier", stop_after_first = true },
+				python = { "black", stop_after_first = true },
+				go = { "gofmt", stop_after_first = true },
+				bash = { "shfmt", stop_after_first = true },
+				zsh = { "shfmt", stop_after_first = true },
+				sh = { "shfmt", stop_after_first = true },
+			},
+		})
 
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function (args)
-                require("conform").format({ bufnr = args.buf })
-            end
-        })
-
-    end
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
