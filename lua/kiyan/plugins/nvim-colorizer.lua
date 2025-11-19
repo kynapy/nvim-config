8f581abbfb85900cf9814983da383b882b508259
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2025-11-19 23:37:27 +0800

    Format code and add comments

--- lua/kiyan/plugins/nvim-colorizer.lua
+++ lua/kiyan/plugins/nvim-colorizer.lua
@@ -1,15 +1,16 @@
 return {
-    'norcalli/nvim-colorizer.lua',
-    config = function ()
-        local colorizer = require('colorizer')
+	-- Used to provide color highlighting for color codes in files
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		local colorizer = require("colorizer")
 
-        colorizer.setup({
-            'css';
-            'javascript';
-            'typescript';
-            html = {
-                mode = 'foreground';
-            }
-        })
-    end
+		colorizer.setup({
+			"css",
+			"javascript",
+			"typescript",
+			html = { mode = "foreground" },
+		})
+
+		vim.keymap.set("n", "<leader>ct", "<cmd>ColorizerToggle<cr>", { desc = "Toggle Colorizer" })
+	end,
 }
