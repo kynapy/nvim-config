e2da7681da41a8fa97654f0fa9c22fa8bc09206e
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2025-11-20 12:40:49 +0800

    Add stylua.toml file for configuration and replace tabs with spaces

--- lua/kiyan/plugins/conform.lua
+++ lua/kiyan/plugins/conform.lua
@@ -1,28 +1,22 @@
 return {
-	-- Formatter plugin for Neovim
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier", stop_after_first = true },
-				typescript = { "prettier", stop_after_first = true },
-				javascriptreact = { "prettier", stop_after_first = true },
-				typescriptreact = { "prettier", stop_after_first = true },
-				python = { "black", stop_after_first = true },
-				go = { "gofmt", stop_after_first = true },
-				bash = { "shfmt", stop_after_first = true },
-				zsh = { "shfmt", stop_after_first = true },
-				sh = { "shfmt", stop_after_first = true },
-			},
-		})
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				require("conform").format({ bufnr = args.buf })
-			end,
-		})
-	end,
+    -- Formatter plugin for Neovim
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettier", stop_after_first = true },
+                typescript = { "prettier", stop_after_first = true },
+                javascriptreact = { "prettier", stop_after_first = true },
+                typescriptreact = { "prettier", stop_after_first = true },
+                python = { "black", stop_after_first = true },
+                go = { "gofmt", stop_after_first = true },
+                bash = { "shfmt", stop_after_first = true },
+                zsh = { "shfmt", stop_after_first = true },
+                sh = { "shfmt", stop_after_first = true },
+            },
+            format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+        })
+    end,
 }
